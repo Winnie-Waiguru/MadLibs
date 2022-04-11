f498b35813158041f14ea771c87123973359460a
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-04-11 18:54:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+story= """
+Roses are {}
+violet is {}
+cake is {}
+And so are you
+"""
+
+color= input("Give me a color: ")
+color2= input("Give me another color: ")
+cake=input ("Give me an adjective ")
+
+print(story.format(color, color2, cake))
